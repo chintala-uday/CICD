194ffc3a6ded851987a0bd32af232c50f334d385
udaya.saikumar@pentair.com <udaya.saikumar@pentair.com>
2026-05-06 18:45:04 +0530

    code changes to verify CICD flow

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
@@ -32,6 +32,7 @@ function AppContent() {
         templateFileName="App.tsx"
         safeAreaInsets={safeAreaInsets}
       />
+      <Text>Hello CICD</Text>
     </View>
   );
 }
